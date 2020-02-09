f4a8a69595f1375f004ad0bee9b4e5ea01d57dfc
Евгений Евгений <ypssyp1@outlook.com>
2020-02-09 20:25:09 +0300

    Добавьте файлы проекта.

---
+++ task02-sort-array.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task02_sort_array.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
